a396c0d76e7f9dc88056351c3d1a60cde69c2ca2
Maksym <maksymRozhko7@gmail.com>
2023-05-15 14:15:02 +0300

    section steps, adaptive

--- src/js/components/scrollAnimation.js
+++ src/js/components/scrollAnimation.js
@@ -15,4 +15,33 @@ const scrollAnimation = endElem => {
     } catch (error) {}
 };
 
-scrollAnimation('.elem .end-elem');
+// scrollAnimation('.elem .elem');
+
+const scrollStepsAnimation = endElem => {
+    const stepsContainer = document.querySelector('.steps .steps__container');
+    const elem = document.querySelector(endElem);
+
+    const observer = new IntersectionObserver(
+        entries => {
+            entries[0].isIntersecting ? elem.classList.add('step--active') : elem.classList.remove('step--active');
+            if (elem.classList.contains('step--active')) {
+                stepsContainer.classList.add(elem.classList[1]);
+            } else {
+                stepsContainer.classList.remove(elem.classList[1]);
+            }
+        },
+        {
+            rootMargin: '-200px',
+        },
+    );
+
+    try {
+        observer.observe(elem);
+    } catch (error) {}
+};
+
+scrollStepsAnimation('.steps .step--one');
+scrollStepsAnimation('.steps .step--two');
+scrollStepsAnimation('.steps .step--three');
+scrollStepsAnimation('.steps .step--four');
+
